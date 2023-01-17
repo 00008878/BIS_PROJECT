d2f16c939605d3c0ec28671c48ddfe592807d098
ulugbek_t <ulugbek_t@corp.aliftech.uz>
2023-01-17 17:14:21 +0500

    created migrations and models

---
+++ app/Models/MibReport.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MibReport extends Model
+{
+    use HasFactory;
+}
